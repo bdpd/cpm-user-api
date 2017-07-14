974b2e445c892098b22013c9c80c186848dbd60d
ddamyanov <dimitar.damyanov@insitex.com>
2017-07-14 16:36:03 +0300

    initial

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # cpm-user-api
+
+test it : url -H "Content-Type: application/json"  -X POST -d '{ "username": "ceco_thebeast", "password": "macarona", "email": "zemi@zaebi.com" }' http://localhost:8080/api/users
+
---
+++ app.js
@@ -0,0 +1,53 @@
+// Express variables
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const port = 8080;
+
+// AWS variables
+const AWS = require('aws-sdk');
+const dynamodb = AWS.DynamoDB({'region': 'us-east-1',apiVersion: "2012-08-10"});
+
+function postUser(userDetails) {
+	if (!userDetails) { 
+		console.log('No user details provided')
+		return {};
+	}
+	var params = {
+		Item: {
+			"Username": {
+		 		S: userDetails.userName
+			}, 
+			"Password": {
+		 		S: userDetails.userPass
+			}, 
+			"Email": {
+		 		S: userDetails.userEmail
+			}
+		}, 
+		ReturnConsumedCapacity: "TOTAL", 
+		TableName: "Users"
+	};
+	dynamodb.putItem(params, function(err, data) {
+	if (err) console.log(err, err.stack);
+	else     console.log(data); 
+	});
+}
+
+
+app.use(bodyParser.json())
+
+app.post('/api/users', function(req, res) {
+    var userDetails = {
+    	userName: req.body.username,
+    	userPass: req.body.password,
+    	userEmail: req.body.email
+    }
+    postUser(userDetails);
+
+    res.send(user_name + ' ' + user_pass + ' ' + user_email);
+});
+
+app.listen(port, function () {
+	console.log('Server started! At http://localhost:' + port);
+});
